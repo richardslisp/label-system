2fd8c732be2a713ca00fdf64cd4e2486b3cc8301
richard shelmerdine <smithfieldlabels@gmail.com>
2021-05-24 16:15:33 +0100

    late commits with codes

--- DO NOT OPEN data sources/Testing/main.el
+++ DO NOT OPEN data sources/Testing/main.el
@@ -2,7 +2,7 @@
 
 (defun print-out ()
   (interactive)
-  (setq titles "company,product,approvedone,approvedtwo,killdays,boxweight,code,colour,slaughtersite,numtoprint")
+  (setq titles "company,product,approvedone,approvedtwo,killdays,code,colour,slaughtersite,numtoprint")
   (setq list-of-info-from-hash-value (return-hash-value-as-list))
   (setq slaughter-site-selected (slaughter-site-select))
   (setq numberoflabelstoprint (alt-num-labels-to-print))
