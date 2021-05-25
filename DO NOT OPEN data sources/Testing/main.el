082bace45b296b232c17e4aa68a36167453ebedb
richard shelmerdine <smithfieldlabels@gmail.com>
2021-05-25 15:38:07 +0100

    got all codes in relatively early for tomrrow, made decent progress in sorting dates directly from within emacs/elisp today!

--- DO NOT OPEN data sources/Testing/main.el
+++ DO NOT OPEN data sources/Testing/main.el
@@ -9,7 +9,9 @@
   (setq rawinfo (concat titles "\n" list-of-info-from-hash-value "," slaughter-site-selected "," numberoflabelstoprint))
   (setq output-string rawinfo)
   (write-region output-string nil "C:/Documents and Settings/S12Despatch.SML/Desktop/NEW LABEL SYSTEM/DO NOT OPEN data sources/Testing/outputfromemacs.txt")
-  (start-process-shell-command "" nil "C:/Documents and Settings/S12Despatch.SML/Desktop/NEW LABEL SYSTEM/DO NOT OPEN data sources/Testing/testbatch.btbat"))
+  (start-process-shell-command "" nil "C:/Documents and Settings/S12Despatch.SML/Desktop/NEW LABEL SYSTEM/DO NOT OPEN data sources/Testing/testbatch.btbat")
+  )
 
 (print-out)
+
 
